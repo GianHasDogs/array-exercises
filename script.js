2f86322998892d8ab9af6521e95f1416cbe36dba
AmyHasTwoDogs <e227601@sbl.ch>
2023-01-17 16:32:08 +0100

    sgcs

--- script.js
+++ script.js
@@ -2,25 +2,29 @@ let beatles = ['John', 'Paul', 'George', 'Ringo']
 document.getElementById('original').innerHTML = beatles
 
 // Exercise 1
-
+beatles = ['Paul', 'Ringo']
+document.getElementById('array-ex1').innerHTML = beatles
 
 
 
 
 // Exercise 2
-
+beatles = ['Paul', 'Ringo', 'Marcin']
+document.getElementById('array-ex2').innerHTML = beatles
 
 
 // Exercise 3 
-
+beatles = ['Paul', 'Ringo', 'Marcin']
+document.getElementById('array-ex3').innerHTML = beatles
+beatles[4] = Ritterhelm
 
 
 // Exercise 4 
-
- beatles
+beatles[10] = Pikachu
+ 
 
 // Exercise 5
-
+document.getElementById('original').innerHTML = 'The array has length '(beatles.length)'
 
 
 // Exercise 6
